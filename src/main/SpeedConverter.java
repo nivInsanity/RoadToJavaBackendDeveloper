6ef485dedc3005929814c944d4a742f52b6130fc
nivInsanity <barzie5554@gmail.com>
2024-12-25 01:58:08 +0100

    Added IDE configuration files and KiloBytes conversion exercise code

---
+++ src/main/SpeedConverter.java
@@ -0,0 +1,23 @@
+package main;
+
+public class SpeedConverter {
+    public static long toMilesPerHour (double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609344);
+    }
+
+    public static String printConversion (double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return "Invalid Value";
+        }
+        else {
+            double milesPerHour = toMilesPerHour(kilometersPerHour);
+
+            return String.format("%.2f km/h = %.2f mi/h", kilometersPerHour, milesPerHour);
+        }
+    }
+
+}
