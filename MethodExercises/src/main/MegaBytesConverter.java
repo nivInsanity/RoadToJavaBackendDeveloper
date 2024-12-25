c3c9e43d2a7df46f655fedce6ed80cf71130096f
nivInsanity <barzie5554@gmail.com>
2024-12-25 02:04:15 +0100

    -Cleaned up duplicated files -zeroTest assertion corrected

---
+++ MethodExercises/src/main/MegaBytesConverter.java
@@ -0,0 +1,15 @@
+package main;
+
+public class MegaBytesConverter {
+    public static String printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            return ("Invalid Value");
+        } else {
+
+            int megaBytes = kiloBytes / 1024;
+            int remainingkiloBytes = kiloBytes % 1024;
+
+            return String.format("%d KB = %d MB and %d KB", kiloBytes, megaBytes, remainingkiloBytes);
+        }
+    }
+}
---
+++ MethodExercises/src/tests/MegaBytesConverterTests.java
@@ -0,0 +1,39 @@
+package tests;
+
+import main.MegaBytesConverter;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MegaBytesConverterTests {
+
+    private static final Logger log = LoggerFactory.getLogger(MegaBytesConverterTests.class);
+
+    @Test
+    public void simpleConvertTest () {
+
+        int kiloBytes = 21374;
+        int megaBytes = kiloBytes / 1024;
+        int remainingKiloBytes = kiloBytes % 1024;
+
+        String conversionMethodMessage = MegaBytesConverter.printMegaBytesAndKiloBytes(kiloBytes);
+        String expectedMessage = String.format("%d KB = %d MB and %d KB", kiloBytes, megaBytes, remainingKiloBytes);
+
+        log.info(conversionMethodMessage);
+
+        Assert.assertEquals(expectedMessage, conversionMethodMessage);
+    }
+
+    @Test
+    public void negativeConvertTest () {
+        int kiloBytes = -1252152512;
+
+        String expectedMessage = "Invalid Value";
+        String conversionMethodMessage = MegaBytesConverter.printMegaBytesAndKiloBytes(kiloBytes);
+
+        log.info(conversionMethodMessage);
+
+        Assert.assertEquals(expectedMessage, conversionMethodMessage);
+    }
+}
