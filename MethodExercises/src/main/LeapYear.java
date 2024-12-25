adc978c124e7bf1cf684491fa000862a86ae03f7
nivInsanity <barzie5554@gmail.com>
2024-12-25 03:33:47 +0100

    -Added LeapYear exercise code

---
+++ MethodExercises/src/main/LeapYear.java
@@ -0,0 +1,9 @@
+package main;
+
+public class LeapYear {
+    public static boolean isLeapYear (int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else return (year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0));
+    }
+}
---
+++ MethodExercises/src/tests/LeapYearTests.java
@@ -0,0 +1,49 @@
+package tests;
+
+import main.LeapYear;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LeapYearTests {
+
+    private static final Logger log = LoggerFactory.getLogger(LeapYearTests.class);
+
+    @Test
+    public void leapYearTest() {
+        int year = 2000;
+
+        boolean isLeapYear = LeapYear.isLeapYear(year);
+
+        log.info(String.valueOf(isLeapYear));
+
+        Assert.assertTrue(isLeapYear);
+    }
+
+    @Test
+    public void notLeapYearTest() {
+        int year = 1900;
+
+        boolean isLeapYear = LeapYear.isLeapYear(year);
+
+        log.info(String.valueOf(isLeapYear));
+
+        Assert.assertFalse(isLeapYear);
+    }
+
+    @Test
+    public void outOfRangeYearTest() {
+        int negativeYear = -5;
+        int yearAboveRange = 10000;
+
+        boolean isLeapYearNegative = LeapYear.isLeapYear(negativeYear);
+        boolean isLeapYearAboveRange = LeapYear.isLeapYear(yearAboveRange);
+
+        log.info(String.valueOf(isLeapYearNegative));
+        log.info(String.valueOf(isLeapYearAboveRange));
+
+        Assert.assertFalse(isLeapYearNegative);
+        Assert.assertFalse(isLeapYearAboveRange);
+    }
+}
