2b8463932a8172d699e06641c4b520ba2653641c
nivInsanity <barzie5554@gmail.com>
2025-02-17 20:22:29 +0100

    -Added diagonal star printer (test only for printing and debugging)

---
+++ ControlFlowChallenges/src/test/java/DiagonalStarTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class DiagonalStarTest {
+
+    @Test
+    public void starPrintTest() {
+
+        //Test only for printing and debugging
+        DiagonalStar.printSquareStar(5);
+    }
+}
